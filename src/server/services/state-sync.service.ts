f1ecca692be105614c784504cd5db6840c0d3503
Михаил Яковенко <cahwar@MacBook-Pro-Mihail.local>
2025-10-24 22:16:13 +0300

    sync separation | skipping payloads patch if player's data hasn't been modified

--- src/server/services/state-sync.service.ts
+++ src/server/services/state-sync.service.ts
@@ -3,6 +3,7 @@ import CharmSync from "@rbxts/charm-sync";
 import { Events } from "server/network";
 import { sharedAtoms } from "shared/sync/state-sync-shared";
 import { filterPayload } from "shared/sync/utils/filter-payload";
+import Object from "@rbxts/object-utils";
 
 @Service()
 export class StateSyncService implements OnStart {
@@ -10,8 +11,33 @@ export class StateSyncService implements OnStart {
 
 	public onStart() {
 		this.syncer.connect((player, payloads) => {
-			warn("server payload for", player.DisplayName, filterPayload(player, payloads));
-			Events.State.SyncState.fire(player, filterPayload(player, payloads));
+			const filtered = filterPayload(player, payloads);
+			const size = Object.keys(filtered.data).size();
+
+			warn("server payload for", player.DisplayName);
+			warn("=>>", filtered);
+			warn("=>> Size:", size);
+
+			const isEmpty =
+				Object.isEmpty(filtered.data) ||
+				(() => {
+					for (const [, v] of pairs(filtered.data)) {
+						if (!typeIs(v, "table")) return false;
+						if (!Object.isEmpty(v)) return false;
+					}
+
+					return true;
+				})();
+
+			warn("=>> Is Empty:", isEmpty);
+
+			if (isEmpty) {
+				warn("=>> Skipping Payload");
+
+				return;
+			}
+
+			Events.State.SyncState.fire(player, filtered);
 		});
 
 		Events.State.RequestSyncState.connect((player) => this.syncer.hydrate(player));
